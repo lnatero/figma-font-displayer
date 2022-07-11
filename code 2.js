a46d99f6a31d15549dfff77deb068b04d35e1767
Lucas <lnatero0@gmail.com>
2022-07-11 00:02:53 -0400

    Archivos copy, 1 y 2 son de prueba. Se genera un estilo pero hay que manejar la funcion async para cargar las fuentes

---
+++ code 2.js
@@ -0,0 +1,38 @@
+const fontStyles = ["Thin", "ExtraLight", "Light", "Regular", "Medium", "SemiBold", "Bold", "ExtraBold", "Black", "Thin Italic", "ExtraLight Italic", "Light Italic", "Regular Italic", "Medium Italic", "SemiBold Italic", "Bold Italic", "ExtraBold Italic", "Black Italic",];
+if (figma.editorType === 'figma') {
+    if (figma.currentPage.selection[0].type === "TEXT") {
+        let fonts = figma.currentPage.selection;
+        console.log(fonts[0].fontName);
+        for (let i = 0; i < fonts.length; i++) {
+            const element = fonts[i];
+            // console.log(element.fontName)
+            function createFontDisplay(figmaElement) {
+                let text = figma.createText();
+                text.x = figmaElement.x;
+                text.y = figmaElement.y + figmaElement.height / 2;
+                text.fontName = figmaElement.fontName;
+                console.log(figmaElement.fontName);
+                // await figma.loadFontAsync({family: "Manrope", style: "Bold"})
+                text.characters = "Almost before we knew it, we had left the ground.";
+                // text.fontSize = element.height
+                // text.fills = [{ type: "SOLID", color: { r: 0, g: 0, b: 0}}]
+            }
+            createFontDisplay(element);
+            // await figma.loadFontAsync(element.fontName)
+            // display.fontName["style"] = "Bold"
+            // for (let i = 0; i < display.FontName.length; i++) {
+            //   const element = display.FontName[i];
+            //   console.log(i)
+            // }
+        }
+        const nodes = [];
+        nodes.push();
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
+        figma.closePlugin();
+    }
+    // If the plugins isn't run in Figma, run this code
+}
+else {
+}
+;
--- code.ts
+++ code.ts
@@ -1,87 +1,74 @@
-// This file holds the main code for the plugin. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
+const fontStyles : string[] =[
+"Thin",
+"ExtraLight",
+"Light",
+"Regular",
+"Medium",
+"SemiBold",
+"Bold",
+"ExtraBold",
+"Black",
 
-// Runs this code if the plugin is run in Figma
+"Thin Italic",
+"ExtraLight Italic",
+"Light Italic",
+"Regular Italic",
+"Medium Italic",
+"SemiBold Italic",
+"Bold Italic",
+"ExtraBold Italic",
+"Black Italic",
+]
+
+const loadFonts = async (_figmaElement) => {
+  await figma.loadFontAsync({family: "Manrope", style: "Bold Italic"})
+  // await figma.loadFontAsync({family: figmaElement.fontName["family"], style: "Bold"})
+  console.log("finished")
+  // hay que hacer un loadFontAsync para cada estilo posible, pero si el estilo no existe, el programa no deja de correr
+  // hay que hacer un timeout y cuando se cumpla se siga al siguiente estilo.
+  // se tienen que cargar todos los estilos y luego generar el texto para todos los estilos encontrados
+  // me parece que la funcion loadFontsAsync es una funcion que funciona con Promises (?) asiq quizas valga la pena revisar
+  // el error handling para las funciones que retornan (o son) Promises.
+}
+function createFontDisplay(figmaElement) {
+  let text = figma.createText()
+  text.x = figmaElement.x
+  text.y = figmaElement.y + figmaElement.height*3/2
+  console.log(figmaElement.fontSize)
+  text.fontName = {family: figmaElement.fontName["family"], style: "Bold Italic"}
+  console.log(figmaElement.fontName)
+  text.characters = "Almost before we knew it, we had left the ground."
+  text.fontSize = figmaElement.fontSize
+}
+
 if (figma.editorType === 'figma') {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many rectangles on the screen.
+  if (figma.currentPage.selection[0].type === "TEXT") {
+    let fonts = figma.currentPage.selection;
+    // console.log(fonts[0].fontName);
+    for (let i = 0; i < fonts.length; i++) {
+      const element = fonts[i];
+      console.log(element.fontName)
+      loadFonts(element).then(() => {
+        createFontDisplay(element)
+        const nodes: SceneNode[] = [];
+        nodes.push();
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
 
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
+        figma.closePlugin();
+      })
 
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < msg.count; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
-      }
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
+      // await figma.loadFontAsync(element.fontName)
+      // display.fontName["style"] = "Bold"
+      // for (let i = 0; i < display.FontName.length; i++) {
+      //   const element = display.FontName[i];
+      //   console.log(i)
+      // }
+
+      
     }
-
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
-  };
-
+  }
 // If the plugins isn't run in Figma, run this code
 } else {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many shapes and connectors on the screen.
 
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
-
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const numberOfShapes = msg.count;
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfShapes; i++) {
-        const shape = figma.createShapeWithText();
-        // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-        shape.shapeType = 'ROUNDED_RECTANGLE'
-        shape.x = i * (shape.width + 200);
-        shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(shape);
-        nodes.push(shape);
-      };
-
-      for (let i = 0; i < (numberOfShapes - 1); i++) {
-        const connector = figma.createConnector();
-        connector.strokeWeight = 8
-
-        connector.connectorStart = {
-          endpointNodeId: nodes[i].id,
-          magnet: 'AUTO',
-        };
-
-        connector.connectorEnd = {
-          endpointNodeId: nodes[i+1].id,
-          magnet: 'AUTO',
-        };
-      };
-
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    }
-
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
   };
-};
